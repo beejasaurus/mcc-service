16d05edf6659d6f2c53a0b651ef39364616334a9
Bryan Neva <bryan.neva@gmail.com>
2017-03-18 19:11:35 -0400

    Add Sample resource

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'cors', 'prefix' => 'v1'], function() {
+
+    Route::resource('samples', 'SampleController');
+
+    Route::get('/', function (Request $request) {
+        return [
+            'api' => [
+                'version' => 1,
+            ],
+        ];
+    });
+});
+
